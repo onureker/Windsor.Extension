fb0b7957821af8ddc201e761ca720786fe6ee4f1
Onur EKER <onur.eker@gmail.com>
2017-02-22 00:16:59 +0300

    ResolveByNameConvention dependency resolver is added

---
+++ Source/Projects/Windsor.Extension/Resolver/ResolveByNameConvention.cs
@@ -0,0 +1,29 @@
+﻿using Castle.Core;
+using Castle.MicroKernel;
+using Castle.MicroKernel.Context;
+using Castle.Windsor;
+
+namespace Windsor.Extension.Resolver
+{
+    public class ResolveByNameConvention<TService>: ISubDependencyResolver
+    {
+        private readonly IWindsorContainer container;
+
+        public ResolveByNameConvention(IWindsorContainer container)
+        {
+            this.container = container;
+        }
+
+        public bool CanResolve(CreationContext context, ISubDependencyResolver contextHandlerResolver, ComponentModel model, DependencyModel dependency)
+        {
+            var result = dependency.TargetType == typeof(TService);
+            return result;
+        }
+
+        public object Resolve(CreationContext context, ISubDependencyResolver contextHandlerResolver, ComponentModel model, DependencyModel dependency)
+        {
+            var result = container.Resolve<TService>(dependency.DependencyKey);
+            return result;
+        }
+    }
+}
