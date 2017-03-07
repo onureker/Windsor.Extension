c79d853a0d4286d704807ce7d3a57907b678ddfa
Onur EKER <onur.eker@gmail.com>
2017-03-07 23:51:20 +0300

    Scope demo improvement

--- Source/Demos/Windsor.Extension.Demo/Sample.Scope/Perspective.cs
+++ Source/Demos/Windsor.Extension.Demo/Sample.Scope/Perspective.cs
@@ -2,7 +2,19 @@
 {
     public class Perspective
     {
-        public static Perspective Debug = new Perspective();
-        public static Perspective Release = new Perspective();
+        public static Perspective Debug = new Perspective("Debug");
+        public static Perspective Release = new Perspective("Release");
+
+        private readonly string name;
+
+        private Perspective(string name)
+        {
+            this.name = name;
+        }
+
+        public override string ToString()
+        {
+            return name;
+        }
     }
 }
